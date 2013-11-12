d2d6aca901f07e0ce54243cb2812c26fab83a7a3
raghav297 <raghav297@Raghavs-MacBook-Pro.local>
2013-11-11 19:37:13 -0800

    Slightly new UI

---
+++ TestDiningHall/src/com/example/testdininghall/HomeActivity.java
@@ -0,0 +1,69 @@
+package com.example.testdininghall;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class HomeActivity extends Activity implements OnClickListener {
+
+	Button diningHall_btn, meal_btn, allMenu_btn, favorite_btn, home_btn;
+    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+   
+        home_btn=(Button)findViewById(R.id.home);
+        home_btn.setOnClickListener(this);
+        
+        diningHall_btn=(Button)findViewById(R.id.diningHall_button);
+        diningHall_btn.setOnClickListener(this);
+        
+        meal_btn=(Button)findViewById(R.id.meal_button);
+        meal_btn.setOnClickListener(this);
+        
+        allMenu_btn=(Button)findViewById(R.id.allMenu_button);
+        allMenu_btn.setOnClickListener(this);
+        
+        favorite_btn=(Button)findViewById(R.id.favorite_button);
+        favorite_btn.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		int clickedBtnId=arg0.getId();
+
+		if (clickedBtnId == R.id.diningHall_button)
+		{
+			Intent ToDiningHall = new Intent (this, DiningHallActivity.class);
+			startActivity(ToDiningHall);
+		}
+		
+		else if (clickedBtnId == R.id.meal_button)
+		{
+			Intent ToMeal1 = new Intent (this, MealActivity.class);
+			startActivity(ToMeal1);
+		}
+		
+		else if (clickedBtnId == R.id.allMenu_button)
+		{
+			Intent ToAllMenu = new Intent (this, AllMenuActivity.class);
+			startActivity(ToAllMenu);
+		}
+		
+		else if (clickedBtnId == R.id.favorite_button)
+		{
+			Intent ToFavorite = new Intent (this, FavoriteActivity.class);
+			startActivity(ToFavorite);
+		}
+		
+	} 
+}
